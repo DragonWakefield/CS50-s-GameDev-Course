7828c8a8f2648ce0921d2f8da55c36977eb57d46
DragonWakefield <dwakefie@uwo.ca>
2020-08-12 10:03:14 -0400

    Dreadhalls submission

---
+++ assignment9/assignment9/Assets/Scripts/ScoreTotal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTotal : MonoBehaviour {
+	public static int score;
+
+	public static bool first = false;
+
+
+
+
+	void Awake() {
+		/*
+		if (instance == null) {
+			score = 0;
+			instance = this;
+		} else if (instance != this) {
+			
+		}
+		*/
+		if (!first) {
+			score = 0;
+			first = true;
+		} else {
+			//score += 1;	
+		}
+	}
+	void Start(){
+
+	}
+
+}
